932a996b53ccd5800ef607b452608e4e29372e2b
vicawil <avdl@gmx.ch>
2015-04-17 16:15:29 +0200

    documentation

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ Usage:
     
 ## Command-line arguments:
 **b**: Treat input file as BEL document, use sequentially incremented
-   number as BEL ID
-   
-**t**: Treat input file as tabulated (CSV), use BEL ID from CSV
+   number as BEL ID.
+  
+**t**: Treat input file as tabulated (CSV), use BEL ID from CSV.
 
 **a**: Only in combination with t: Do not include sentence Id and PMID 
    as passage infons.
+
+**n**: Map entity symbol or ID (field `value`) to unique internal 
+   BEL identifier (field `BID`). Note: Equivalence files must be placed 
+   in /equivalence_files and registered in equivalence_files.json .
--- csv_helpers.rb
+++ csv_helpers.rb
@@ -42,13 +42,13 @@ Usage: bel2csv.rb -<args> <files>
 --------------------------------------------------------------------
 Command-line arguments:
 b: Treat input file as BEL document, use sequentially incremented
-   number as BEL ID
-t: Treat input file as tabulated (CSV), use BEL ID from CSV
+   number as BEL ID.
+t: Treat input file as tabulated (CSV), use BEL ID from CSV.
 a: Only in combination with t: Do not include sentence Id and PMID 
    as passage infons.
 n: Map entity symbol or ID (field `value`) to unique internal 
    BEL identifier (field `BID`). Note: Equivalence files must be placed 
-   in /equivalence_files and registered in `equivalence_files.json`
+   in /equivalence_files and registered in `equivalence_files.json`.
 --------------------------------------------------------------------
 	EOS
 	puts string
